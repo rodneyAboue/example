1ebea19295b83a6a5a37b03d9c9f52d125872324
kenneth aboue <rodneyabouek@gmail.com>
2022-11-23 15:24:28 +0100

    fix:error xmake

--- example/xmake.lua
+++ example/xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 target("example")
     set_kind("binary")
-    add_files("src/*.c
+    add_files("src/*.c")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
