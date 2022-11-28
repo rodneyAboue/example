1b83ed556905faf22b944adf9535e37df440fdb2
rodneyaboue <62360031+rodneyaboue@users.noreply.github.com>
2022-11-28 10:28:49 +0100

    Update xmake.lua

--- example/xmake.lua
+++ example/xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release")
 target("example")
     set_kind("binary")
     add_files("src/*.cpp")
-
+-- new test
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
 --
