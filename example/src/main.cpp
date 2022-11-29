ffadc3bdf9a4ba6eb219b7e2d1760e71349f0299
rodneyaboue <62360031+rodneyaboue@users.noreply.github.com>
2022-11-29 11:45:23 +0100

    Update main.cpp

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include "openmp.h>
 using namespace std;
 
 int main(int argc, char** argv)
 {
     cout << "hello world!: Kenneth Abouedf fvzdczedazd" << endl;
+    #pragma omp parallel {
+    cout<<omp_get_thread_num()<<endl;
+    }
     return 0;
 }
