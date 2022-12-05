10d3b3408d809d666dafc356e96ce11759ef698c
Elias <98809496+eblbn@users.noreply.github.com>
2022-12-05 14:27:32 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "commit Elias" << endl;
+    cout << "commit Elias 14h27" << endl;
     // cout << "kenneth" << endl;
     return 0;
 }
